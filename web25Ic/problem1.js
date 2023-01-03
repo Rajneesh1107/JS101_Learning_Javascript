a253742e0b254df33f8cb53e7debbd24b9fecc5e
unknown <rajnishyadav11072002@gmail.com>
2023-01-03 20:12:28 +0530

    String

---
+++ web25Ic/problem1.js
@@ -0,0 +1,7 @@
+let story="Hi My name is Rajneesh Yadav. I am coming from Jaunpur Uttar Pradesh";
+let count=0;
+let spaces=" ";
+for (i=0; i<story.length; i++){
+    count+=1;
+  }
+console.log("Total Char in arr Story =",count);
